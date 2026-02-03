6d9387d5b142d428d2e6d50d8c3d628772d7e722
JordiIbanezMasip <jordimasip2003@gmail.com>
2026-02-03 19:23:53 +0100

    agregado por el momento fragement

--- app/src/main/java/com/example/examenkotlin/fragment_tascas.kt
+++ app/src/main/java/com/example/examenkotlin/fragment_tascas.kt
@@ -1,32 +1,20 @@
 package com.example.examenkotlin
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var boton: Button
-
+class fragment_tascas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_fragment_tascas)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        boton = findViewById<Button>(R.id.botton)
-
-        boton.setOnClickListener {
-            val intent = Intent(this, fragment_tascas::class.java)
-            startActivity(intent)
-        }
     }
-
 }
