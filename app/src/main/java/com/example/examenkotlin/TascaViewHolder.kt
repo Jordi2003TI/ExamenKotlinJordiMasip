9726a95df864a561090284f0633c3e14d19ba42b
JordiIbanezMasip <jordimasip2003@gmail.com>
2026-02-03 20:05:30 +0100

    añadir colores

--- app/src/main/java/com/example/examenkotlin/TascaViewHolder.kt
+++ app/src/main/java/com/example/examenkotlin/TascaViewHolder.kt
@@ -1,11 +1,22 @@
 package com.example.examenkotlin
 
 import android.view.View
+import android.widget.TextView
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
 class TascaViewHolder(
     itemView: View
 ): RecyclerView.ViewHolder(itemView) {
+    private var tvNom: TextView =itemView.findViewById(R.id.tvNom)
+    private var tvCategoria: TextView = itemView.findViewById(R.id.tvCategoria)
+    private var tvData: TextView = itemView.findViewById(R.id.tvData)
+    private var tvEstat: TextView = itemView.findViewById(R.id.tvEstat)
+
+    fun bind(item: Tasca{
+
+    }
 
 
 }
